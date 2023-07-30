c98604428073b71008d2e0667d47a9b7709be486
DmitriiLZUTKM <89247303456@mail.ru>
2023-07-30 14:03:41 +1000

    //ДЗ

---
+++ Task 19/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая принимает на вход пятизначное
+// число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+static bool IsPalindrome(string input)
+{
+        int length = input.Length;
+        for (int i = 0; i < length / 2; i++)
+        {
+            if (input[i] != input[length - i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+}
+
+Console.Write("Введите слово или фразу: ");
+        string input = Console.ReadLine();
+
+        if (IsPalindrome(input))
+        {
+            Console.WriteLine("Да, это палиндром.");
+        }
+        else
+        {
+            Console.WriteLine("Нет, это не палиндром.");
+        }
+
+        Console.ReadLine();
---
+++ Task 21/Program.cs
@@ -0,0 +1,38 @@
+﻿//Напишите программу, которая принимает на вход
+//координаты двух точек и находит расстояние между 
+//ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+    {
+        Console.WriteLine("Введите координаты точки A:");
+        Console.Write("x: ");
+        double ax = double.Parse(Console.ReadLine());
+        Console.Write("y: ");
+        double ay = double.Parse(Console.ReadLine());
+        Console.Write("z: ");
+        double az = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите координаты точки B:");
+        Console.Write("x: ");
+        double bx = double.Parse(Console.ReadLine());
+        Console.Write("y: ");
+        double by = double.Parse(Console.ReadLine());
+        Console.Write("z: ");
+        double bz = double.Parse(Console.ReadLine());
+
+        double distance = CalculateDistance(ax, ay, az, bx, by, bz);
+        Console.WriteLine("Расстояние между точкой A и B: " + distance.ToString("F2"));
+    }
+
+    static double CalculateDistance(double ax, double ay, double az, double bx, double by, double bz)
+    {
+        double dx = ax - bx;
+        double dy = ay - by;
+        double dz = az - bz;
+
+        double distance = Math.Sqrt(dx * dx + dy * dy + dz * dz);
+
+        return distance;
+    }
